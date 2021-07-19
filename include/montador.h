d2164362af5d3a4e797183cca2f86a838b10909a
AndreOGodoy <godoy.andre@hotmail.com>
2021-07-19 11:52:49 -0300

    Adds codes for instructions and registers

--- include/montador.h
+++ include/montador.h
@@ -10,6 +10,40 @@
 
 #include <iostream>
 
+const static std::map<std::string, std::string> _REGISTERS = {
+        {"R0", "0"},
+        {"R1", "1"},
+        {"R2", "2"},
+        {"R3", "3"},
+};
+
+const static std::map<std::string, std::string> _CODES = {
+        { "HALT", "0"},
+        { "LOAD", "1"},
+        { "STORE", "2"},
+        { "READ", "3"},
+        { "WRITE", "4"},
+        { "COPY", "5"},
+        { "PUSH", "6"},
+        { "POP", "7"},
+        { "ADD", "8"},
+        { "SUB", "9"},
+        { "MUL", "10"},
+        { "DIV", "11"},
+        { "MOD", "12"},
+        { "AND", "13"},
+        { "OR", "14"},
+        { "NOT", "15"},
+        { "JUMP", "16"},
+        { "JZ", "17"},
+        { "JN", "18"},
+        { "CALL", "19"},
+        { "RET", "20"},
+        {"END", ""},
+        {"WORD", ""},
+};
+
+
 // TODO: structs, classes, e outras definições.
 using LINE_TOKEN = std::string;
 using TOKEN = std::string;
